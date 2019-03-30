52a44c91a5481fadeea115644cdbbf3c6e923beb
Tom Meredith <tommeredith01@gmail.com>
2019-03-30 19:47:20 -0400

    try different username

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
-const url = 'mongodb+srv://tommeredith:Alaska%3DMDB2@chipsndip-ntlni.mongodb.net/test'
+const url = 'mongodb+srv://tommeredith01%40gmail%2Ecom:Alaska%3DMDB2@chipsndip-ntlni.mongodb.net/test?retryWrites=true'
 
 module.exports = {
     url
